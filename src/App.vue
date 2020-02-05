2e45168dc18c8788554deceba69a5ed76c4fe328
fanzhikang <455983827@qq.com>
2020-02-05 20:30:00 +0800

    add frame

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,24 @@
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    //引用局部组件
+  },
+  //当前组件使用的数据
+  data: {
+    name: 'hahaha'
+  },
+  methods: {
+    //写方法
+  },
+  computed: {
+    //计算属性
+  },
+  //生命周期
+  created: {
+
+  }
 }
 </script>
 
---
+++ src/Register.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>注册页面</div>
+</template>
+
+<script>
+export default {
+    data: function() {
+return {
+
+    };
+},
+    components: {
+
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    created: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
